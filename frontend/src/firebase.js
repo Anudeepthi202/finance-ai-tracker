d00179cab8a8612bbfa4fcd1077194aa6dac3438
Anu Deepthi <anudeepthi202@gmail.com>
2025-09-02 15:16:06 +0530

    key files changed

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjedObptPrasfhI2B1Gg3wEeuehmQz6dw",
-  authDomain: "authentication-da098.firebaseapp.com",
-  projectId: "authentication-da098",
-  storageBucket: "authentication-da098.firebasestorage.app",
-  messagingSenderId: "424758073544",
-  appId: "1:424758073544:web:c1e9c67b680994c463adfd",
-  measurementId: "G-DXV839KTJN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
